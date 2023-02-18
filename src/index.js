127731a127677b271e3fe60e9d7baa742134c602
HafizAzeem <hafizazeemk@gmail.com>
2023-02-18 22:14:16 +0500

    netfliex series

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render( 
-        <>
-        <h1>Hallow World</h1>
-        <p>Please Like and follow to thapa </p>
-        <p>test</p>
-         <ul>
-                <li>A1</li>
-                <li>A2</li>
-                <li>A3</li>
-         </ul>
+ReactDOM.render(
+        <>    
+           <h1>Netflix Pick</h1>
+           <p>Favirout netflix series</p>
+           <ol>
+                <li>Dark </li>
+                <li>Extra </li>
+                <li>first2 love </li>
+                <li>Robot </li>
+                <li>Holo </li>
+           </ol>
         </>
-        
-        ,document.getElementById('root') )
-      
+, document.getElementById('root'));
