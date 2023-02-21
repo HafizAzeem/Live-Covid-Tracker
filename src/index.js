eb2702ce2c3c8915d2b0303469eb53b84bc1f605
HafizAzeem <hafizazeemk@gmail.com>
2023-02-21 23:10:37 +0500

    javascript template literals backticks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 const azeem= 'Azeem is a web developer '
+const sss= 'tets'
 ReactDOM.render( 
         
         <>
 
-                <h1> hi {azeem} </h1>
+                <h1> { `kkkkkk ${azeem}  ${sss}`} </h1>
                 <p> Expresion= Operands and expression {4+5} </p>
+                <p> { `i am learning ${azeem}` }</p>
         </>
         ,document.getElementById('root')
 
