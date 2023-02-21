28d377bd83f74bb29eeae3e43486f3657a79a054
HafizAzeem <hafizazeemk@gmail.com>
2023-02-22 00:42:55 +0500

    jsx attribute

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
-const date= new Date();
-let day= date.getDate()
-let month=date.getMonth()+1
-let year= date.getFullYear()
-let hours= date.getHours()
-let mintues=date.getMinutes()
-let seconds=date.getSeconds()
-
+import React from "react";
+import ReactDOM from "react-dom"
 ReactDOM.render(
         <>
-           <h1>My name is azeem </h1> 
-           <p>{`Today date is ${day}-${month}-${year}`}</p>    
-           <p>{`Now time is ${hours}:${mintues}:${seconds} `}</p>    
+                <h1 contentEditable="true">Azeem</h1>
+                <img src="https://picsum.photos/200/300" alt="Rnadom Image" />
         </>,
         document.getElementById('root')
 )
