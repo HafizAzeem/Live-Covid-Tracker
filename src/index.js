5a54ce18399ba2c432acd667fe83ad752be825fb
HafizAzeem <hafizazeemk@gmail.com>
2023-02-21 23:52:11 +0500

    display date time

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM  from "react-dom";
-const azeem= 'Azeem is a web developer '
-const sss= 'tets'
-ReactDOM.render( 
-        
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+const date= new Date();
+let day= date.getDate()
+let month=date.getMonth()+1
+let year= date.getFullYear()
+let hours= date.getHours()
+let mintues=date.getMinutes()
+let seconds=date.getSeconds()
+
+ReactDOM.render(
         <>
-
-                <h1> { `kkkkkk ${azeem}  ${sss}`} </h1>
-                <p> Expresion= Operands and expression {4+5} </p>
-                <p> { `i am learning ${azeem}` }</p>
-        </>
-        ,document.getElementById('root')
-
-);
+           <h1>My name is azeem </h1> 
+           <p>{`Today date is ${day}-${month}-${year}`}</p>    
+           <p>{`Now time is ${hours}:${mintues}:${seconds} `}</p>    
+        </>,
+        document.getElementById('root')
+)
