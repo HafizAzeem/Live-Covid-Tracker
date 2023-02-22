43bf9368fbdecaba5e80d8d3e828b0c4bb6da445
HafizAzeem <hafizazeemk@gmail.com>
2023-02-22 22:50:52 +0500

    greetings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom"
-import './index.css'
-const heading= {
-         fontSize:'25px',
-         color:'blue',
-}
+import ReactDOM from "react-dom";
+
+let currentDate= new Date(2022,2,22,15);
+
+  currentDate= currentDate.getHours();
+  let greetings=''
+  const cssStyle= {
+
+  }
+  if(currentDate>= 1 && currentDate< 12){
+        greetings= 'Good Morning'
+        cssStyle.color='green'
+  }else if(currentDate>=12 && currentDate<16){
+        greetings= 'Good After Noon'
+        cssStyle.color='orange'
+  }else{
+        greetings= "Good Night"
+        cssStyle.color='black'
+  }
 ReactDOM.render(
         <>
-                <h1 style={heading}>Azeem</h1>
-                <h1 style={{ textTransform:"uppercase" }}> Azeem 2</h1>
-                <img src="https://picsum.photos/200/300" alt="Rnadom Image" />
+           <h1>Hello Sir, <span style={cssStyle}>{greetings}</span></h1>
         </>,
         document.getElementById('root')
-)
+);
