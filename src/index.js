50bf0ef1f640856efb3a993d0600c385e72134e8
HafizAzeem <hafizazeemk@gmail.com>
2023-02-22 22:15:28 +0500

    inline styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom"
+import './index.css'
+const heading= {
+         fontSize:'25px',
+         color:'blue',
+}
 ReactDOM.render(
         <>
-                <h1 contentEditable="true">Azeem</h1>
+                <h1 style={heading}>Azeem</h1>
+                <h1 style={{ textTransform:"uppercase" }}> Azeem 2</h1>
                 <img src="https://picsum.photos/200/300" alt="Rnadom Image" />
         </>,
         document.getElementById('root')
